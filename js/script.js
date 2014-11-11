2cd3caf46343fac07c8f4b608a06a4e38b99e991
maryelehner <mary@whoisstudio.com>
2014-11-11 16:02:56 -0500

    d

--- js/script.js
+++ js/script.js
@@ -1,44 +1,52 @@
-
-  //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-  function autoPlayYouTubeModal() {
-      var trigger = $("body").find('[data-toggle="modal"]');
-      trigger.click(function () {
-          var theModal = $(this).data("target"),
-              videoSRC = $(this).attr("data-theVideo"),
-              videoSRCauto = videoSRC + "?autoplay=1";
-          $(theModal + ' iframe').attr('src', videoSRCauto);
-          $(theModal + ' button.close').click(function () {
-              $(theModal + ' iframe').attr('src', videoSRC);
-          });
-          $('.modal').click(function () {
-              $(theModal + ' iframe').attr('src', videoSRC);
-          });
-      });
-  }
-
-$(window).load(function(){
-    var $container = $('.employees');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
- 
+$(window).load(function(){ 
     $('.filters a').click(function(){
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    }); 
-    autoPlayYouTubeModal();
+        if($(this).hasClass('selected')){
+           $(this).removeClass('selected');
+        }
+        else{
+            $(this).addClass('selected');
+        }   
+    });
+    $('.position-button').click(function(){
+        if($(this).hasClass('highlight')){
+           $(this).removeClass('highlight');
+        }
+        else{
+            $(this).addClass('highlight');
+        }   
+    });
+    
+$('#myCarousel').carousel({
+  interval: false,
+  pause: true
+})
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+    
+$(".see-all").click(function () {
+    if($(".job-results").hasClass("in")) {
+        $('.see-all').text("See all");
+    } else {
+        $('.see-all').text("See less");
+    }
+});    
+    
+
+
 });
 
